df53e7d7282a4d35928ecfd9bada4978c031fcf8
hans.vanderlaan <j.h.vanderlaan@student.utwente.nl>
2020-04-17 14:37:06 +0200

    Add examples + small fixes

--- calendar/__init__.py
+++ calendar/__init__.py
@@ -1,10 +1,6 @@
 import itertools
-from itertools import chain
-
 import math
-
 import portion as P
-
 
 class Calendar:
 
@@ -15,27 +11,25 @@ class Calendar:
         self.basecalendar = basecalendar
         self.periods = period
         self.period = sum(self.periods)
-        self.items = dict()
 
     def __getitem__(self, key: int):
         if self.basecalendar is None:
             return P.closed(key, key)
         else:
-            if key in self.items.keys():
-                return self.items[key]
-            else:
-                passed_periods = [self.periods[x % len(self.periods)] for x in range(0, key)]
-                p_key = self.reftime + sum(passed_periods)
-                #print("p:" + str(p_key))
-                n_key = self.periods[key % len(self.periods)]
-                #print("n:" + str(n_key))
+            turns = divmod(key, len(self.periods))
+            passed_periods_time = turns[0] * sum(self.periods) #How many times have all periods been passed?
+            passed_periods_time += sum([self.periods[x % len(self.periods)] for x in range(0, turns[1])])
 
-                c_key = P.closed(self.basecalendar[p_key].lower,self.basecalendar[p_key + n_key].lower)
-                c_key = list(P.iterate(c_key, step=1, base=int))
-                c_key = P.closed(c_key[0], c_key[-1]-1)
+            p_key = self.reftime + passed_periods_time
+            #print("p:" + str(p_key))
+            n_key = self.periods[key % len(self.periods)]
+            #print("n:" + str(n_key))
 
-                self.items[key] = c_key
-                return c_key
+            c_key = P.closed(self.basecalendar[p_key].lower,self.basecalendar[p_key + n_key].lower)
+            c_key = list(P.iterate(c_key, step=1, base=int))
+            c_key = P.closed(c_key[0], c_key[-1]-1)
+
+            return c_key
 
 class PeriodicExpression:
     def __init__(self, name: str, starting_points: list, calendars:list, duration, duration_calendar):
@@ -55,20 +49,20 @@ class PeriodicExpression:
         l = l.lower
         #equal to:
         # l = index_C(key, len(self.calendars)-1, self.starting_points, self.calendars)
-        u = l + self.duration
-        return P.closed(self.calendars[-1][l].lower, self.calendars[-1][u].upper)
+        u = self.calendars[len(self.calendars)-1][ticks(key, len(self.calendars)-1, self.starting_points, self.calendars) + self.duration - 1].upper
+        return P.closed(l, u)
 
 def ticks(i, alpha, startingpoints, calendars):
     sp = startingpoint(i, alpha, startingpoints, calendars)
     if alpha == 0:
-        print("i=" + str(i) + " alpha=" + str(alpha) + " c[" + str(sp) + "]")
+        #print("i=" + str(i) + " alpha=" + str(alpha) + " c[" + str(sp) + "]")
         return sp
     else:
         periods = calendars[alpha - 1].periods
         returnval = startingpoints[alpha][sp] \
                + sum([periods[x % len(periods)] for x in range(0, ticks(i, alpha - 1, startingpoints, calendars))])\
                + calendars[alpha - 1].reftime
-        print("i=" + str(i) + " alpha=" + str(alpha) + " c[" + str(returnval) + "]")
+        #print("i=" + str(i) + " alpha=" + str(alpha) + " c[" + str(returnval) + "]")
         return returnval
 
 def startingpoint(i, alpha, startingpoints, calendars):
@@ -82,7 +76,7 @@ def startingpoint(i, alpha, startingpoints, calendars):
 
 def valid(startingpoints, calendars):
         if len(calendars) == 1:
-            return startingpoints[0]
+            return [startingpoints[0]]
         else:
             product = list(itertools.product(startingpoints[0],valid(startingpoints[1:], calendars[1:])))
             result = []
@@ -94,38 +88,3 @@ def valid(startingpoints, calendars):
                     if(calendars[0].periods[combination[0] % len(calendars[0].periods)] > combination[1][0]):
                         result.append(list(combination))
             return result
-
-    # def calculate_(self, amount):
-    #     intervals = list()
-    #     for calendar_number in range(1, len(self.starting_points)):
-    #         intervals.append(list())
-    #         for starting_point in range(0, len(self.starting_points[calendar_number])):
-    #             for count in range(0,amount + 5 ** calendar_number):
-    #                 intervals[calendar_number-1].append(
-    #                 self.calendars[calendar_number]
-    #                 [self.starting_points[calendar_number][starting_point] + self.calendars[calendar_number].period * count].lower)
-    #     #for i in range(0, len(intervals)):
-    #         #for j in range(0, len(intervals[i])):
-    #             #intervals[i][0] = intervals[i][0].union(intervals[i][j])
-    #         #print(intervals[i][0])
-    #     return intervals
-
-# def X(i: int, n:int, starting_points:list):
-#     #print(i,n)
-#     return math.floor( i /
-#                        max(1,sum([len(starting_points[x]) for x in range(n+1, len(starting_points))]))
-#                       ) \
-#            % len(starting_points[n])
-#
-# def Counts(i,n, starting_points, calendars):
-#     if n == 0:
-#         r_val =  math.floor( i /
-#                        max(1,sum([len(starting_points[x]) for x in range(n+1, len(starting_points))]))
-#                       )
-#         print("number of C0: " + str(r_val) + calendars[0].name)
-#         return r_val
-#     else:
-#         r_val =  starting_points[n][X(i,n,starting_points)] \
-#            + sum([calendars[n-1].periods[x % len(calendars[n-1].periods)] for x in range (0, Counts(i, n-1, starting_points, calendars) )])
-#         print("number of C" + str(n) + ":" + str(r_val) + calendars[n].name)
-#         return r_val
--- calendar/main.py
+++ calendar/main.py
@@ -1,27 +1,117 @@
 from calendar import Calendar, PeriodicExpression
 
+#elemental calendars
 c_basic = Calendar('basic', 0, None, [1])
 c_minutes = Calendar('minutes', 0, c_basic, [1])
 c_hours = Calendar('hours', 0, c_minutes, [60])
 c_days = Calendar('days', 0, c_hours, [24])
 c_weeks = Calendar('weeks', 0, c_days, [7])
-print(c_basic[10])
-print(c_minutes[12])
-print(c_hours[0])
-print(c_hours[1])
-print(c_days[0])
-print(c_days[1])
+c_months = Calendar('months', 0, c_days, [31,28,31,30,31,30,31,31,30,31,30,31,
+                                          31,28,31,30,31,30,31,31,30,31,30,31,
+                                          31,29,31,30,31,30,31,31,30,31,30,31,
+                                          31,28,31,30,31,30,31,31,30,31,30,31])
+c_years = Calendar('years', 0, c_months, [12])
 
+# print(c_months[0])
+# print(c_weeks[1])
+# print(c_weeks[1])
+# print(c_days[9])
+# print(c_hours[224])
+# print(c_minutes[13470])
+# print(c_days[0])
+# print(c_days[1])
+
+#some basic periodic expressions
 pe1 = PeriodicExpression("pe1", [[0],[0,2,3],[0,2,6],[0,30]], [c_weeks, c_days, c_hours, c_minutes], 1, c_minutes)
-pe2 = PeriodicExpression("business_hours", [["all"],[0,1,2,3,4],[8],[30]], [c_weeks, c_days, c_hours, c_minutes], 510, c_minutes)
-print(pe1[0])
-print(pe1[1])
-print(pe1[2])
-print(pe1[3])
-print(pe1[4])
-print(pe1[5])
-print(pe1[6])
-print(pe1[7])
-print(pe1[8])
-print(pe2[7])
-#print(pe1.calculate_(10))
+pe_bh = PeriodicExpression("business_hours", [["all"],[0,1,2,3,4],[8],[30]], [c_weeks, c_days, c_hours, c_minutes], 510, c_minutes)
+
+# print("---- PE-BH ----")
+# print("pe_bh[0]", pe_bh[0])
+# print("pe_bh[1]", pe_bh[0])
+# print("pe_bh[2]", pe_bh[2])
+# print("pe_bh[3]", pe_bh[3])
+# print("pe_bh[4]", pe_bh[4])
+# print("pe_bh[5]", pe_bh[5])
+# print("pe_bh[6]", pe_bh[6])
+# print("pe_bh[7]", pe_bh[7])
+# print("pe_bh[8]", pe_bh[8])
+
+print("---- PE-BH Example ----")
+print("pe_bh[8]", pe_bh[8])
+print("c_weeks[1]", c_weeks[1])
+print("c_days[9]", c_days[9])
+print("c_hours[224]", c_hours[224])
+print("c_minutes[13470]", c_minutes[13470])
+print("c_minutes[13979]", c_minutes[13979])
+
+#the basic periodic expressions of the trebla case
+pe_wh = PeriodicExpression("Working Hours", [["all"],[0,1,2,3,4],[8],[30]], [c_weeks, c_days, c_hours, c_minutes], 510, c_minutes)
+pe_ov = PeriodicExpression("Overtime", [["all"],[0,1,2,3,4],[17],[00]], [c_weeks, c_days, c_hours, c_minutes], 90, c_minutes)
+pe_vh = PeriodicExpression("Visiting Hours", [["all"],[0,1,2,3,4],[10]], [c_weeks, c_days, c_hours], 6, c_hours)
+pe_gs = PeriodicExpression("Guarding Shifts", [["all"],[0,1,2,3,4,5,6],[18],[30]], [c_weeks, c_days, c_hours, c_minutes], 780, c_minutes)
+pe_cs = PeriodicExpression("Cleaning Shifts", [["all"],[2,4],[18],[30]], [c_weeks, c_days, c_hours, c_minutes], 90, c_minutes)
+
+print("c_years[30]", c_years[30])
+
+#determine min_i for wh
+# min_i_wh = pe_wh[7827]
+# min_i_wh_lower = pe_wh[7826]
+# print(c_years[30].contains(min_i_wh.lower))
+# print(c_years[30].contains(min_i_wh_lower.lower))
+
+print("pe_wh[7827]", pe_wh[7827])
+print("pe_wh[7828]", pe_wh[7828])
+print("pe_wh[7829]", pe_wh[7829])
+print("pe_wh[7830]", pe_wh[7830])
+print("pe_wh[7831]", pe_wh[7831])
+
+#determine min_i for ov
+# min_i_ov = pe_ov[7827]
+# min_i_ov_lower = pe_ov[7826]
+# print(c_years[30].contains(min_i_wh.lower))
+# print(c_years[30].contains(min_i_wh_lower.lower))
+
+print("pe_ov[7827]", pe_ov[7827])
+print("pe_ov[7828]", pe_ov[7828])
+print("pe_ov[7829]", pe_ov[7829])
+print("pe_ov[7830]", pe_ov[7830])
+print("pe_ov[7831]", pe_ov[7831])
+
+#determine min_i for vh
+# min_i_vh = pe_vh[7827]
+# min_i_vh_lower = pe_vh[7826]
+# print(c_years[30].contains(min_i_vh.lower))
+# print(c_years[30].contains(min_i_vh_lower.lower))
+
+print("pe_vh[7827]", pe_vh[7827])
+print("pe_vh[7828]", pe_vh[7828])
+print("pe_vh[7829]", pe_vh[7829])
+print("pe_vh[7830]", pe_vh[7830])
+print("pe_vh[7831]", pe_vh[7831])
+
+#determine min_i for gs
+# min_i_gs = pe_gs[10957]
+# min_i_gs_lower = pe_gs[10956]
+# print(min_i_gs)
+# print(c_years[30].contains(min_i_gs.lower))
+# print(c_years[30].contains(min_i_gs_lower.lower))
+
+print("pe_gs[10957]", pe_gs[10956])
+print("pe_gs[10957]", pe_gs[10957])
+print("pe_gs[10958]", pe_gs[10958])
+print("pe_gs[10959]", pe_gs[10959])
+print("pe_gs[10960]", pe_gs[10960])
+print("pe_gs[10961]", pe_gs[10961])
+print("pe_gs[10962]", pe_gs[10962])
+print("pe_gs[10963]", pe_gs[10963])
+print("pe_gs[10964]", pe_gs[10964])
+
+#determine min_i for cs
+# min_i_cs = pe_cs[3130]
+# min_i_cs_lower = pe_cs[3129]
+# print(min_i_cs)
+# print(c_years[30].contains(min_i_cs.lower))
+# print(c_years[30].contains(min_i_cs_lower.lower))
+
+print("pe_cs[3130]", pe_cs[3130])
+print("pe_cs[3131]", pe_cs[3131])
